d39f5a5b22a3363358962124ee3711b23ddb44b5
ColtonMiddaugh <MiddaughCS20@gcc.edu>
2022-11-11 13:18:11 -0500

    Add Dice and Board Class

--- src/Board.java
+++ src/Board.java
@@ -1,22 +1,22 @@
 import java.util.ArrayList;
 
 public class Board {
-    private ArrayList<Player> players = new ArrayList<Players>();
-    private ArrayList<Enemy> enemies = new ArrayList<Enemies>();
+    private ArrayList<Players> players = new ArrayList<Players>();
+    private ArrayList<Enemies> enemies = new ArrayList<Enemies>();
 
-    public void addPlayer(Player player){
+    public void addPlayer(Players player){
         players.add(player);
     }
 
-    public void addEnemy(Enemy enemy){
+    public void addEnemy(Enemies enemy){
         enemies.add(enemy);
     }
 
-    public void removePlayer(Player player){
+    public void removePlayer(Players player){
         players.remove(player);
     }
 
-    public void removeEnemy(Enemy enemy){
-        enemies.remove(enemy)
+    public void removeEnemy(Enemies enemy){
+        enemies.remove(enemy);
     }
 }
