ea2d48982583911dfed3aa81daac584e3a58329d
ANASTASIBM18 <AnastasiBM18@gcc.edu>
2022-11-14 17:48:11 -0500

    Javadoc for archer, players, slime, warrior and weapons class - Ben

--- src/Archer.java
+++ src/Archer.java
@@ -1,7 +1,31 @@
+/**
+ *This class is for the enemy character Archer who has a long rang attack and a close range attack
+ */
 public class Archer extends Enemies {
+    /**
+     * Constructor for the archer
+     * @param health - the amount of health the Archer will start with
+     * @param mana - the amount of mana the Archer will start with
+     * @param speed - the amount of speed the Archer will start with
+     * @param armor - the strength of armor the Archer will start with
+     * @param name - the name of the Archer
+     * @param w - the type of weapon the character has
+     */
     public Archer(int health, int mana, int speed, int armor, String name,Weapons w) {
         super(health, mana, speed, armor, w,name);
     }
+
+    /**
+     * This method is for the short range, low strength melee attach
+     * @param dieRoll - the roll of the die which will determine if the attack hits
+     * @return - the amount of damage dealt
+     */
     public int stab (int dieRoll){ return -1; }
+
+    /**
+     * This method is for the long range attack of the Archer
+     * @param dieRoll - the roll of the die which will determine if the attack hits
+     * @return - the amount of damage dealt
+     */
     public int shoot (int dieRoll){ return -1; }
 }
--- src/Weapons.java
+++ src/Weapons.java
@@ -1,24 +1,47 @@
+/**
+ * The weapons class allows the user to create any type of weapon of any type of weapon strength
+ * Then these weapons are used by the individual characters in the game
+ */
 public class Weapons {
     private String weaponType;
     private int weaponStrength;
     private Dice dmgDie;
     private int dmgModifier;
+
+    /**
+     * This si the constructor for the weapons class
+     * @param type - type of weapon, for example "sword"
+     * @param strength - how much max damage the weapon can deal
+     * @param numSidesForDice - how many sides the dice will have which is used to determine how much damage
+     *                        the weapon will deal
+     */
     public Weapons (String type,int strength,int numSidesForDice){
         weaponType = type;
         weaponStrength = strength;
         dmgDie = new Dice(numSidesForDice);
     }
 
+    /**
+     * Copy constructor for the weapons class
+     * @param w - takes in an already existing weapon and creates a copy
+     */
     public Weapons (Weapons w) {
         this.weaponType = w.weaponType;
         this.weaponStrength = w.weaponStrength;
         this.dmgDie = new Dice(w.dmgDie);
     }
 
+    /**
+     * Gets the type of an already existing weapon
+     * @return - the type of an already existing weapon
+     */
     public String getWeaponType(){
         return weaponType;
     }
-
+    /**
+     * Gets the strength of an already existing weapon
+     * @return - the strength of an already existing weapon
+     */
     public int getWeaponStrength(){
         return weaponStrength;
     }
