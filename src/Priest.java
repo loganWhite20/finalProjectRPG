bdede006d3fac2ecfb3be507a5959be1aa9b4a99
ANASTASIBM18 <AnastasiBM18@gcc.edu>
2022-12-06 13:50:40 -0500

    Created the logic for the indHeal method for the Priest - Ben

--- src/Priest.java
+++ src/Priest.java
@@ -1,8 +1,13 @@
+import java.util.Random;
+
 public class Priest extends Enemies {
     public Priest(int health, int mana, int speed, int armor, String name,Weapons w,String t) {
         super(health, mana, speed, armor, name, w,t);
     }
-    public int indHeal(int dieRoll){ return -1; }
+    public int indHeal(){
+        Random dieRoll = new Random();
+        return dieRoll.nextInt(10) + 4;
+    }
     public int indMana(int dieRoll){ return -1; }
     public int groupHeal(int dieRoll){ return -1; }
     public int groupMana(int dieRoll){ return -1; }
